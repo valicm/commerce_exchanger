01d0746ae351c04f71545753bbc523db9f8bb7ea
Valentino Međimorec <valentino@medjimorec.net>
2019-09-24 09:16:22 +0200

    List builder informations

--- src/ExchangeRatesListBuilder.php
+++ src/ExchangeRatesListBuilder.php
@@ -17,6 +17,7 @@ class ExchangeRatesListBuilder extends ConfigEntityListBuilder {
     $header['label'] = $this->t('Exchange rates');
     $header['id'] = $this->t('Machine name');
     $header['mode'] = $this->t('Mode');
+    $header['status'] = $this->t('Status');
     return $header + parent::buildHeader();
   }
 
@@ -28,6 +29,7 @@ class ExchangeRatesListBuilder extends ConfigEntityListBuilder {
     $row['label'] = $entity->label();
     $row['id'] = $entity->id();
     $row['mode'] = $configuration['mode'];
+    $row['status'] = $entity->status() ? $this->t('Enabled') : $this->t('Disabled');
     // You probably want a few more properties here...
     return $row + parent::buildRow($entity);
   }
